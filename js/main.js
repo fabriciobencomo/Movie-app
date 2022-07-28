d4319847576c72f911892cb3903a08b8678bc0d5
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-07-27 23:31:45 -0400

    axios migration and genres endpoint used

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-const URL_POSTER_IMG_300 = 'https://image.tmdb.org/t/p/w300/'
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+      'api_key': API_KEY,
+    },
+  });
 
-const URL_TRENDING_MOVIES =  'https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY;
+const URL_POSTER_IMG_300 = 'https://image.tmdb.org/t/p/w300'
+
+const URL_TRENDING_MOVIES =  'trending/movie/day' ;
+const URL_GENRE_MOVIES = 'genre/movie/list';
 
 async function getTrendingMovies() {
-    const res = await fetch(URL_TRENDING_MOVIES);
-    const data = await res.json();
+    const { data } = await api(URL_TRENDING_MOVIES);
     
     const movies = data.results;
 
@@ -25,4 +35,28 @@ async function getTrendingMovies() {
     })
 }
 
+async function getGenres() {
+    const { data } = await api(URL_GENRE_MOVIES);
+    
+    const genres = data.genres;
+
+    const categoriesPreviewList = document.querySelector('.categoriesPreview-list');
+
+    genres.forEach(genre => {
+        const genresContainer = document.createElement('div');
+        genresContainer.classList.add('category-container');
+
+        const genreTitle = document.createElement('h3');
+        genreTitle.classList.add('category-title');
+        genreTitle.setAttribute('id', 'id' + genre.id);
+        const genreTitleText = document.createTextNode(genre.name);
+
+        genreTitle.appendChild(genreTitleText);
+        genresContainer.appendChild(genreTitle);
+        categoriesPreviewList.appendChild(genresContainer);
+
+    })
+}
+
 getTrendingMovies();
+getGenres();
